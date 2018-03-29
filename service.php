efeb69fbf71ddca9f8d384b90ef6414b7e5e25b8
kumahacker <kumahavana@gmail.com>
2018-03-29 23:02:47 +0200

    update una nota por persona

--- service.php
+++ service.php
@@ -625,12 +625,12 @@ class Pizarra extends Service
 				(SELECT COUNT(note) FROM _pizarra_actions WHERE note=A.id AND email='{$profile->email}' AND action='unlike') > 0 AS isunliked
 			FROM (
 				SELECT * FROM _pizarra_notes subq2 INNER JOIN (
-					SELECT max(id) as idx FROM _pizarra_notes
+					SELECT max(inserted) as insertedx FROM _pizarra_notes
 					WHERE (topic1='$topic' OR topic2='$topic' OR topic3='$topic')
-					GROUP BY email, idx
+					GROUP BY email
 					ORDER BY inserted DESC
 					LIMIT 500) subq 
-				ON subq.idx = subq2.id
+				ON subq.insertedx = subq2.inserted
 			) A
 			LEFT JOIN person B ON A.email = B.email
 			JOIN _pizarra_users C ON A.email = C.email");
