27a2054fcd6425d99acb543af48c1cefa3de5a08
Salvi Pascual <salvi.pascual@gmail.com>
2020-11-17 15:25:28 -0500

    add message in ''

--- service.php
+++ service.php
@@ -286,7 +286,7 @@ class Service
 		// run powers for amulet VIDENTE
 		if (Amulets::isActive(Amulets::VIDENTE, $note->id_person) && $type === 'note') {
 			$msg = "Los poderes del amuleto del Druida te avisan: A @{$request->person->username} le disgustó tu nota en Pizarra";
-			Notifications::alert($note->id_person, $msg, 'remove_red_eye', "{command:'PIZARRA NOTA', data:{note:'{$request->input->data->note}'}}");
+			Notifications::alert($note->id_person, $msg, 'remove_red_eye', "{'command':'PIZARRA NOTA', 'data':{'note':'{$request->input->data->note}'}}");
 		}
 	}
 
